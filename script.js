cd5b4bc461b64e772b3f8eaa2e4ed37b228685bf
JUNG\alswj <20212856@office.deu.ac.kr>
2022-12-30 18:25:14 +0900

    변수, 상수, 배열 학습

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("hi");
+const num = 3;
+const name = "John";
+let name2 = "John";
+
+console.log(num-1);
+console.log(num+1);
+console.log(num/1);
+console.log(num*1);
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(arr);
+
+arr.push(11);
+
+arr.forEach((item) => {
+    console.log(item);
+});
