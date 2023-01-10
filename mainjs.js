07935cffc845e9dc87ecbaeeb21cd8187da9bcd1
JUNG\alswj <20212856@office.deu.ac.kr>
2023-01-10 21:54:28 +0900

    로컬 스토리지 사용하여 유저 네임 저장

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,4 @@
-const loginform = document.querySelector(".login_form");
+const loginform = document.querySelector("#login_form");
 const loginInput = loginform.querySelector("input");
 const loginbtn = loginform.querySelector("button");
 
@@ -23,35 +23,6 @@ function loginbtnClick(event)
 
 loginbtn.addEventListener("click", loginbtnClick);
 
-
-// HTML을 통해 조건을 주지 않고 글자수, 공백 확인
-const loginform2 = document.querySelector("#form");
-const loginInput2 = loginform.querySelector("input");
-
-
-function onLoginSubmit(event)
-{
-    event.preventDefault(); // 기본적으로 발생하는 이벤트를 막는다.
-    const username2 = loginInput2.value;
-    if(username2 == "")
-    {
-        alert("아이디를 입력해주세요.");
-    }
-    else if(username2.length < 15)
-    {
-        alert("15자 이내로 입력하세요.");
-        
-    }
-    else
-    {
-        alert("반갑습니다. " + username2); 
-    }
-
-}
-
-loginform2.addEventListener("submit", onLoginSubmit);
-
-
 const link = document.querySelector("a");
 function handleLinkClick(event)
 {
@@ -60,3 +31,41 @@ function handleLinkClick(event)
 }
 
 link.addEventListener("click", handleLinkClick);
+
+
+// HTML을 통해 조건을 주지 않고 글자수, 공백 확인
+const loginform2 = document.querySelector("#form");
+const loginInput2 = loginform2.querySelector("input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_C = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event)
+{
+    event.preventDefault(); // 기본적으로 발생하는 이벤트를 막는다.
+    const username2 = loginInput2.value;
+
+    loginform2.classList.add(HIDDEN_C);
+    paintGreetings(username2);
+    localStorage.setItem(USERNAME_KEY, username2);
+}
+
+loginform2.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username)
+{
+    greeting.innerText = `${username} 님, 환영합니다.`;
+    greeting.classList.remove(HIDDEN_C);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername == null)
+{
+    loginform2.classList.remove(HIDDEN_C);
+    loginform2.addEventListener("submit", onLoginSubmit);
+}
+else
+{
+    paintGreetings(savedUsername);
+}
