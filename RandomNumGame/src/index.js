2262cea508aa6c3d78a9438317e37dd88f9c0779
JUNG <97083703+Jung2312@users.noreply.github.com>
2023-01-30 18:36:51 +0900

    Add files via upload

--- RandomNumGame/src/index.js
+++ RandomNumGame/src/index.js
@@ -8,17 +8,29 @@ function getRandomt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
 }
 
-function playEventhandler(event) {
-  event.preventDefault();
-  const maxNum = document.querySelector("#maxNum").value;
-  const userNum = document.querySelector("#userNum").value;
-  const random = getRandomt(0, maxNum);
-
-  message.innerHTML = `You chose: ${userNum}, the machine chose: ${random}`;
+function getResult(userNum, random) {
   if (parseInt(userNum) === random) {
     result.innerHTML = "You won!";
   } else {
     result.innerHTML = "You lost!";
+  }
+}
+
+function playEventhandler(event) {
+  event.preventDefault();
+  const maxNum = document.querySelector("#maxNum").value;
+  const userNum = document.querySelector("#userNum").value;
+
+  if (userNum === "" || maxNum === "") {
+    alert("숫자를 모두 입력하세요.");
+  } else if (userNum < 0 || maxNum < 0) {
+    alert("정수를 입력하세요.");
+  } else if (userNum > maxNum) {
+    alert("범위보다 작은 값을 입력하세요.");
+  } else {
+    const random = getRandomt(0, maxNum);
+    message.innerHTML = `You chose: ${userNum}, the machine chose: ${random}`;
+    getResult(userNum, random);
   }
 }
 
